b59992dba26035dc9c66348fa5831a01519ad391
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-10-16 23:00:15 -0300

    creating hash pass and helpers

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ const environments = {}
 environments.stage = {
   'httpPort': 3000,
   'httpsPort': 3001,
-  'envName': 'staging'
+  'envName': 'staging',
+  'hashingSecret': 'asudha8sdg7asd807agd87agd87gas78d'
 }
 
 // Prod environments
 environments.production = {
   'httpPort': 5000,
   'httpsPort': 5001,
-  'envName': 'production'
+  'envName': 'production',
+  'hashingSecret': 'ma0sjd90a8sdtas76d87g231fv23'
 }
 
 const currentEnvironment = typeof (process.env.NODE_ENV) == 'string' ?
